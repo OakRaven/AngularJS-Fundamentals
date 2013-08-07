e64166bf6c2964ce664a7910aaa0959ff33ee832
Mike Hatfield <oakraven13@gmail.com>
2013-08-07 15:50:42 -0300

    Initial load.

---
+++ app/js/app.js
@@ -0,0 +1,6 @@
+var eventsApp = {};
+
+(function(){
+  'use strict';
+  eventsApp = angular.module('eventsApp', ['ngSanitize']);
+}());
---
+++ app/js/controllers/EditEventController.js
@@ -0,0 +1,7 @@
+(function(){
+  'use strict';
+  eventsApp.controller('EditEventController',
+    function EditEventController($scope){
+    }
+  );
+})();
