51efb551c73a0a7898b7e0a8f0a552876e1e772e
Mike Hatfield <oakraven13@gmail.com>
2013-08-08 13:31:24 -0300

    Added SublimeText project. Added new event data JSON files. Updated eventData service to load event data via $http.

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -4,7 +4,7 @@
   eventsApp.controller('EventController',
     function EventController($scope, eventData) {
       $scope.sortOrder = "name";
-      $scope.event = eventData.event;
+      $scope.event = eventData.getEvent();
 
       $scope.upVoteSession = function(session){
         session.upVoteCount++;
