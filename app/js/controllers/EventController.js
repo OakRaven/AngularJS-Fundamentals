473d5539a6a926aad3c67c1b2533148b6b85cdee
Mike Hatfield <oakraven13@gmail.com>
2013-08-15 15:23:58 -0300

    Completed section on query parameters.

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -2,9 +2,10 @@
   'use strict';
 
   eventsApp.controller('EventController',
-    function EventController($scope, $routeParams, eventData) {
+    function EventController($scope, $routeParams, eventData, $route) {
       $scope.sortOrder = "name";
       $scope.event = eventData.getEvent($routeParams.eventId);
+      console.log($route.current.params.eventId);
 
       $scope.upVoteSession = function(session){
         session.upVoteCount++;
@@ -12,6 +13,10 @@
 
       $scope.downVoteSession = function(session){
         session.upVoteCount--;
+      };
+
+      $scope.reload = function(){
+        $route.reload();
       };
     }
   );
