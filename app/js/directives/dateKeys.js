43314c0c4d3cc4f2cbb7c4984084177c3ab1c18a
Mike Hatfield <oakraven13@gmail.com>
2013-08-19 22:08:25 -0300

    Added new gravatar image directive.

--- app/js/directives/dateKeys.js
+++ app/js/directives/dateKeys.js
@@ -5,8 +5,13 @@
     return {
       restrict: 'A',
       link: function(scope, element, attrs, controller){
-        element.on('keydown', function(evt){
-
+        element.on('keydown', function(event){
+          if(isNumericKeyCode(event.keyCode) ||
+            isForwardSlashKeyCode(event.keyCode) ||
+            isNavigationKeyCode(event.keyCode)){
+            return true;
+          }
+          return false;
         });
       }
     };
@@ -22,6 +27,7 @@
     function isNavigationKeyCode(keyCode){
       switch(keyCode){
         case 8:   // backspace
+        case 9:   // tab
         case 35:  // end
         case 36:  // home
         case 37:  // left
